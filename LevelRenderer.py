ad109a2719790791ae55ce115b42a8ad07c9528e
EmberLynn <ember.robb@gmail.com>
2022-11-16 20:42:16 -0700

    Working on expception printing for failure of properly formatted Level file.

--- LevelRenderer.py
+++ LevelRenderer.py
@@ -9,6 +9,7 @@
 
 import os
 import pygame
+import traceback
 from Block import Block
 
 absolute_path = os.path.dirname(__file__)
@@ -23,7 +24,7 @@ class LevelRenderer:
         # final intent is to read all levels in the level folder
         block_section = False
         file = open(full_path + "\Level1.txt", "r")
-        for line in file:
+        for linenum, line in enumerate(file, 1):
             
             # handle general properties  
             if('SCREEN_WIDTH' in line):
@@ -33,16 +34,29 @@ class LevelRenderer:
             elif('LEVEL_COLOR' in line):
                 var = line.split('=')
                 self.level_color = eval(var[1])
+            elif('BLOCK_WIDTH' in line):
+                self.block_width = int(''.join(filter(str.isdigit,line)))
+            elif('BLOCK_HEIGHT' in line):
+                self.block_height = int(''.join(filter(str.isdigit,line)))
 
             # handle block groups
             if('#blocks_section' in line):
                 block_section = True
+                continue
             if('#end_of_blocks' in line):
                 block_section = False
+                continue
+            try:
+                if(block_section):
+                    # do some error checking first
+                    # based on line length, block width and screen width, can this line be rendered?
+                    if(self.block_width > (self.screen_width/(len(line)-1))):
+                        raise Exception("Your blocks are too wide given the screen width and column request!")
+            except Exception as e:
+                print(e)
+                print("Problem located in" + file.name + " at " + line + " Line number:" + str(linenum))
+                quit() # we can't go on!
 
-            if(block_section):
-                # do some error checking first
-                # based on line length, block width and screen width, can this line be rendered?
-                print("There was an error")
+
              
             
